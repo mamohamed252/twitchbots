66802d9a9e932f79d064159adb850479d753a118
mamohamed252 <ma.mohamed252@gmail.com>
2022-01-26 22:21:01 -0600

    connected translatebot

---
+++ mos-bot/mosBot.js
@@ -0,0 +1,64 @@
+const tmi = require('tmi.js');
+const Secrets = require('./secrets.js');
+const LangBot = require('./other-bot-commands/langBot.js');
+// Define configuration options
+const opts = {
+  identity: {
+    username: Secrets.twitchInfo.identity.username,
+    password: Secrets.twitchInfo.identity.password,
+  },
+  channels: [
+    Secrets.twitchChannels[0],
+    // Secrets.twitchChannels[1],
+    // Secrets.twitchChannels[2],
+    // Secrets.twitchChannels[3],
+    // Secrets.twitchChannels[4],
+    // Secrets.twitchChannels[5]
+  ],
+    // Automatic reconnection
+    connection: { reconnect: true }
+};
+
+// Create a client with our options
+const client = new tmi.client(opts);
+
+// Register our event handlers (defined below)
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+
+// Connect to Twitch:
+client.connect();
+
+// Called every time a message comes in
+function onMessageHandler(channel, userState, msg, self) {
+  if (self) { return; } // Ignore messages from the bot
+
+  msg = msg.trim();
+  if (msg[0] != '!') return; //if first character in msg is not an exclamation stop program
+
+  const commandArray = msg.split(" ");
+
+  // Remove whitespace from chat message
+  const commandName = commandArray[0];
+  console.log('commandArray', commandArray)
+
+  // If the command is known, let's execute it
+  if (commandName === '!dice') {
+    const num = rollDice();
+    client.say(channel, `${userState.username} rolled a ${num}`);
+    console.log(`* Executed ${commandName} command`);
+  } else if (commandName === '!tr') {
+    LangBot.languageBotCommands(client, channel, userState, msg, self, commandArray)
+  }
+}
+
+// Function called when the "dice" command is issued
+function rollDice() {
+  const sides = 6;
+  return Math.floor(Math.random() * sides) + 1;
+}
+
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler(addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+}
---
+++ mos-bot/other-bot-commands/langBot.js
@@ -0,0 +1,108 @@
+// API Google Translate
+const gtrans = require('googletrans').default;
+
+const tr_lang = {
+  'de': ['de', 'says'],
+  'en': ['en', 'says'],
+  'fr': ['fr', 'says'],
+  'pt': ['pt', 'says'],
+  'es': ['es', 'says'],
+  'pa': ['pa', 'says'],
+  'zu': ['zu', 'says'],
+  'ar': ['ar', 'says'],
+  'tl': ['tl', 'says'], 
+}
+
+// Called every time a message comes in
+function languageBotCommands(client, target, context, msg, self, commandArray) {
+  try {
+
+    // Ignore messages from the bot
+    if (self) { return; }
+
+    // Remove whitespace from chat message
+    let tMsg = msg.trim();
+
+    // Check if the message starts with @name
+    // in that case, extract the name and move the @name at the end of the message, and process
+    if (tMsg[0] === '@') {
+      let atnameEndIndex = tMsg.indexOf(' ');
+      let atname = tMsg.substring(0, atnameEndIndex);
+      let msg = tMsg.substring(atnameEndIndex + 1);
+      tMsg = msg + ' ' + atname;
+      console.info('Changed message :', tMsg);
+    }
+
+    // Filter commands (options)
+    if (commandArray[1][0] != '!') return; // if first character in second command is not an exclamation stop program
+    
+    // Extract command
+    let cmd = commandArray[1].substring(1).toLowerCase(); // removes the exclamation from command
+
+    // Name for answering
+    let answername = '@' + context['display-name'];
+
+    // Command for displaying the commands (in english)
+    if (cmd === "lang" || cmd === "translate") {
+      client.say(target, 'I can (approximatevely) translate your messages in many languages. Simply start your message with one of these commands: !en (english) !fr (french)  !de (german) !pt (portuguese)... ');
+      return;
+    }
+
+   
+
+    // Commands for displaying messages explaining the translation feature in various languages
+    // TODO: sentences
+    const explanations = {
+      //    'germans': '',
+      //    'spanish': '',
+      'french': 'Vous pouvez utiliser notre bot traducteur. Commencez votre message par !en pour traduire votre message en anglais. Par exemple "!en Bonjour"',
+    }
+    if (cmd in explanations) {
+      client.say(target, explanations[cmd]);
+      return;
+    }
+
+    if (cmd in tr_lang) {
+      var ll = tr_lang[cmd];
+      //console.error(ll);
+      var txt = tMsg.substring(commandArray[0].length + commandArray[1].length + 2); // removes first two commands from the translated message
+
+      // Text must be at least 2 characters and max 200 characters
+      var lazy = false;
+      if (txt.length > 2) {
+        if (txt.length > 200) {
+          lazy = true;
+          txt = "i'm too lazy to translate long sentences ^^";
+        }
+
+        // Lazy mode, and english target => no translation, only displays 'lazy' message in english
+        if ((lazy === true) && (ll[0].indexOf('en') == 0)) {
+          say(target, context['display-name'] + ', ' + txt);
+          return;
+        }
+        
+
+        // Translate text
+        gtrans(txt, { to: ll[0] }).then(res => {
+          if (lazy === true) {
+            // lazy mode sentence in english and also in requested language
+            client.say(target, context['display-name'] + ', ' + txt + '/' + res.text);
+          }
+          else {
+            // Translation
+            // TODO: Check is translated text == original text. In that case it
+            // means the command was not correctly used (ex: "!en hello friends")
+            client.say(target, context['display-name'] + ' ' + ll[1] + ': ' + res.text);
+          }
+        }).catch(err => {
+          console.error('Translation Error:', err);
+        })
+      }
+    }
+  }
+  catch (e) {
+    console.error(e.stack);
+  }
+}
+
+exports.languageBotCommands = languageBotCommands
